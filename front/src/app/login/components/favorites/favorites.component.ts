6f2d54a56cf6d43e9b43382e7f93ee75a51391c5
Alexander KIRILOV <alexander.kirilov@devoteam.com>
2019-02-21 18:01:44 +0100

    Added some styles

--- front/src/app/login/components/favorites/favorites.component.ts
+++ front/src/app/login/components/favorites/favorites.component.ts
@@ -2,7 +2,8 @@ import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-favorites',
-  templateUrl: './favorites.component.html'
+  templateUrl: './favorites.component.html',
+  styleUrls: ['./favorites.components.scss']
 })
 export class FavoritesComponent implements OnInit {
 
--- front/src/app/login/components/login-page/login-page.component.ts
+++ front/src/app/login/components/login-page/login-page.component.ts
@@ -12,71 +12,72 @@ const CLIENT_ID = 'cognito-client-id';
 const AUTH_FLOW = 'cognito-auth-flow';
 
 @Component({
-    selector: 'app-login-page',
-    templateUrl: 'login-page.component.html'
+  selector: 'app-login-page',
+  templateUrl: 'login-page.component.html',
+  styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
-    user: User;
-    authInfo: AuthInfo;
+  user: User;
+  authInfo: AuthInfo;
 
-    result = '';
-    error = '';
-    private loggedInUser;
+  result = '';
+  error = '';
+  private loggedInUser;
 
-    ngOnInit(): void {
-        this.user = new User();
-        this.authInfo = new AuthInfo();
+  ngOnInit(): void {
+    this.user = new User();
+    this.authInfo = new AuthInfo();
 
-        this.user.email = localStorage.getItem(EMAIL) || '';
-        this.authInfo.identityPoolId = localStorage.getItem(IDENTITY_POOL) || '';
-        this.authInfo.userPoolId = localStorage.getItem(USER_POOL) || '';
-        this.authInfo.region = localStorage.getItem(REGION) || '';
-        this.authInfo.clientId = localStorage.getItem(CLIENT_ID) || '';
+    this.user.email = localStorage.getItem(EMAIL) || '';
+    this.authInfo.identityPoolId = localStorage.getItem(IDENTITY_POOL) || '';
+    this.authInfo.userPoolId = localStorage.getItem(USER_POOL) || '';
+    this.authInfo.region = localStorage.getItem(REGION) || '';
+    this.authInfo.clientId = localStorage.getItem(CLIENT_ID) || '';
 
-        this.authInfo.authenticationFlowType = localStorage.getItem(AUTH_FLOW) || '';
-    }
+    this.authInfo.authenticationFlowType = localStorage.getItem(AUTH_FLOW) || '';
+  }
 
-    register() {
+  register() {
 
-    }
+  }
 
-    changePassword() {
-        console.log('Change First Time Password');
-        Auth.completeNewPassword(this.loggedInUser, this.authInfo.password, null)
-            .then(data => console.log(data))
-            .catch((error) => {
-                console.log(error);
-                this.error = JSON.stringify(error);
-            });
-    }
+  changePassword() {
+    console.log('Change First Time Password');
+    Auth.completeNewPassword(this.loggedInUser, this.authInfo.password, null)
+      .then(data => console.log(data))
+      .catch((error) => {
+        console.log(error);
+        this.error = JSON.stringify(error);
+      });
+  }
 
-    login() {
-        Amplify.configure({
-            Auth: {
-                // identityPoolId: this.authInfo.identityPoolId,
-                region: this.authInfo.region,
-                userPoolId: this.authInfo.userPoolId,
-                userPoolWebClientId: this.authInfo.clientId,
-                authenticationFlowType: this.authInfo.authenticationFlowType,
-            }
-        });
+  login() {
+    Amplify.configure({
+      Auth: {
+        // identityPoolId: this.authInfo.identityPoolId,
+        region: this.authInfo.region,
+        userPoolId: this.authInfo.userPoolId,
+        userPoolWebClientId: this.authInfo.clientId,
+        authenticationFlowType: this.authInfo.authenticationFlowType,
+      }
+    });
 
-        Auth.signIn(this.user.email, this.user.password)
-            .then(
-            (user) => {
-                console.log(user);
-                this.loggedInUser = user;
+    Auth.signIn(this.user.email, this.user.password)
+      .then(
+        (user) => {
+          console.log(user);
+          this.loggedInUser = user;
 
-                if (user.challengeName === 'NEW_PASSWORD_REQUIRED') {
-                    this.error = 'This user needs to change their password. See field below.';
-                } else {
-                    this.result = JSON.stringify(user.signInUserSession, null, 2);
-                }
+          if (user.challengeName === 'NEW_PASSWORD_REQUIRED') {
+            this.error = 'This user needs to change their password. See field below.';
+          } else {
+            this.result = JSON.stringify(user.signInUserSession, null, 2);
+          }
 
-            })
-            .catch((err) => {
-                console.log(err);
-                this.error = JSON.stringify(err);
-            });
-    }
+        })
+      .catch((err) => {
+        console.log(err);
+        this.error = JSON.stringify(err);
+      });
+  }
 }
